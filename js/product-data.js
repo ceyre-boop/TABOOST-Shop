0012182e0c521baafcbeeb3dbace3371376e11f6
colin eyre <ceyre@mcc.edu>
2026-04-08 16:04:42 -0400

    Meticulous update of product prices from Column E (Sale Price) in tap-products.csv

--- js/product-data.js
+++ js/product-data.js
@@ -1,5 +1,5 @@
 // TABOOST Discovery Platform - Product & Campaign Data Pipeline
-// Generated: 2026-04-08T15:58:56.540587
+// Generated: 2026-04-08T16:03:37.404800
 // Total Products: 3451 | Active Campaigns: 3451
 // Unique de-duped names: 3451
 
